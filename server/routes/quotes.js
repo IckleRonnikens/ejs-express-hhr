2b74411d69de7225825cdf36d6e90f4b79149c51
IckleRonnikens <76468847+IckleRonnikens@users.noreply.github.com>
2026-03-26 11:56:27 +1100

    quotes

--- server/routes/quotes.js
+++ server/routes/quotes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 module.exports = (param) => {
 
     const { quotesService } = param; 
-    // const { writersService } = param; 
+    // const { psService } = param; 
     // const { archivesService } = param; 
     // const { publicService } = param; 
 
@@ -17,7 +17,13 @@ module.exports = (param) => {
     });
 
 
+    router.get('/ps', async(req, res, next) => {
 
+        const quoteslist = await quotesService.getList();
+        const allStories = await quotesService.getAllStories();
+        return res.render('quotes/ps', {page: "Philospher's Stone", pslist, stories: allStories});
+
+    });
  
 
 
